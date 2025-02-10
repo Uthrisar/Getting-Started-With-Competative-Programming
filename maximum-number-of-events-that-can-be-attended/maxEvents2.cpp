5c0cdf567d3579e11562c77491be1f1e42490b40
UthrisarKhungurBoro <121308655+Uthrisar@users.noreply.github.com>
2025-02-10 21:43:38 +0530

    Update maxEvents2.cpp

--- maximum-number-of-events-that-can-be-attended/maxEvents2.cpp
+++ maximum-number-of-events-that-can-be-attended/maxEvents2.cpp
@@ -22,18 +22,18 @@ public:
             endPoints.push_back(events[i][1]);
         }
         vector<vector<int>> dp(n,vector<int>(k+1,-1));
-        return f(n-1,k,endPoints,events,dp);
+        return rec(n-1,k,endPoints,events,dp);
     }
-    int f(int ind,int k,vector<int>& endPoints,vector<vector<int>>& events,vector<vector<int>>& dp){
+    int rec(int ind,int k,vector<int>& endPoints,vector<vector<int>>& events,vector<vector<int>>& dp){
         if(k == 0) return 0;
         if(ind < 0) return 0;
         if(dp[ind][k] != -1) return dp[ind][k];
-        int ans = f(ind-1,k,endPoints,events,dp);
+        int ans = rec(ind-1,k,endPoints,events,dp);
         auto it = upper_bound(endPoints.begin(),endPoints.end(),events[ind][0]);
         if(it != endPoints.begin()){
             it--;
             int j = distance(endPoints.begin(),it);
-            ans = max(ans,f(j,k-1,endPoints,events,dp)+events[ind][2]);
+            ans = max(ans,rec(j,k-1,endPoints,events,dp)+events[ind][2]);
         } else {
             ans = max(ans,events[ind][2]);
         }
